2b3bd80141eeeccde528dfa8798fd4505eba83ad
HalaszGergo2 <xlomox12@gmail.com>
2025-01-21 16:13:24 +0100

    Lablec

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navigacio from '../components/Navigacio'
+import Lablec from '../components/Lablec'
 
 function Layout() {
     return (
             < >
                 <Navigacio />
                 <Outlet />
+                <Lablec />
             </>
     )
 }
