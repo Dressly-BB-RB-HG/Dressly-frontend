7c07873047cec86c45c5b117674cc5527a691ee0
baksabence <gxdcloud@gmail.com>
2025-01-28 14:36:40 +0100

    termekek formazas, kedvenc gomb

--- src/components/Termek.js
+++ src/components/Termek.js
@@ -1,21 +1,31 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { KosarContext } from '../contexts/KosarContext'
-
+import './Termek.css'
 
 function Termek(props) {
-  const {kosarbaTesz}=useContext(KosarContext)
+  const { kosarbaTesz } = useContext(KosarContext)
+  const [kedvenc, setKedvenc] = useState(false);
 
-  
+  const kedvencKezelo = () => {
+    setKedvenc(!kedvenc);
+  };
 
   return (
-    <div className="card col-md-4 mb-4"> 
-            <div className="card-body">
-                <img className="kep card-img-top" alt={props.adat.tipus + props.adat.kategoria} src={props.adat.kep} />
-                <h4 className="card-text">{props.adat.gyarto}</h4>
-                <p className="card-text">{props.adat.ar}</p>
-                <button className="gomb btn btn-primary mt-4" onClick={() => kosarbaTesz(props.data)}>Kosárba tesz</button>
-            </div>
-       </div>
+    <div className="card col-md-4 mb-4">
+      <div className="card-body">
+        <img className="kep card-img-top" alt={props.adat.tipus + props.adat.kategoria} src={props.adat.kep} />
+        <h4 className="gyarto card-text">{props.adat.gyarto}</h4>
+        <p className="ar card-text">{props.adat.ar}</p>
+        <div className="gombok">
+          <button className="kosarbagomb btn btn-primary mt-4" onClick={() => kosarbaTesz(props.data)}>Kosárba tesz</button>
+          <button className={`kedvenc-gomb ${kedvenc ? 'kedvenc-aktiv' : ''}`}
+          onClick={kedvencKezelo}
+          >
+            {kedvenc ? '♥' : '♡'}
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
