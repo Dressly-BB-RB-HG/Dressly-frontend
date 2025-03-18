fd16428e345be87362818f74bc57f33d8592f897
HalaszGergo2 <xlomox12@gmail.com>
2025-03-18 14:21:00 +0100

    kezdolap elkezdve

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Modell from './pages/AdminOldalak/Modell';
 import Rendelesek from './pages/AdminOldalak/Rendelesek';
 import FelhasznaloRendeles from './pages/FelhasznaloRendeles';
 import RendelesOldal from './pages/RendelesOldal';
+import { ApiProvider } from './contexts/ApiContext';
 
 
 function App() {
@@ -22,21 +23,23 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <KosarProvider>
+        <ApiProvider>
           <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Kezdolap />} />
-              <Route path="Bejelentkezes" element={<Bejelentkezes />} />
-              <Route path="regisztracio" element={<Regisztracio />} />
-              <Route path="ruhazat" element={<Ruhazat />} />
-              <Route path="profil" element={<Profil />} />
-              <Route path="admin" element={<Admin />} />
-              <Route path="felhasznalok" element={<Felhasznalok />} />
-              <Route path="modell" element={<Modell />} />
-              <Route path="rendelesek" element={<Rendelesek />} />
-              <Route path="felhasznalorendelesek" element={<FelhasznaloRendeles />} />
-              <Route path="rendelesoldal" element={<RendelesOldal />} />
-            </Route>
-          </Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Kezdolap />} />
+                <Route path="Bejelentkezes" element={<Bejelentkezes />} />
+                <Route path="regisztracio" element={<Regisztracio />} />
+                <Route path="ruhazat" element={<Ruhazat />} />
+                <Route path="profil" element={<Profil />} />
+                <Route path="admin" element={<Admin />} />
+                <Route path="felhasznalok" element={<Felhasznalok />} />
+                <Route path="modell" element={<Modell />} />
+                <Route path="rendelesek" element={<Rendelesek />} />
+                <Route path="felhasznalorendelesek" element={<FelhasznaloRendeles />} />
+                <Route path="rendelesoldal" element={<RendelesOldal />} />
+              </Route>
+            </Routes>
+          </ApiProvider>
         </KosarProvider>
        </AuthProvider>
     </BrowserRouter>
