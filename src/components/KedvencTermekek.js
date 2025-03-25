24c41e813789e97c928da6c050537897dda9b98d
HalaszGergo2 <xlomox12@gmail.com>
2025-03-25 22:44:22 +0100

    kedvencek kinezet

--- src/components/KedvencTermekek.js
+++ src/components/KedvencTermekek.js
@@ -7,9 +7,11 @@ function KedvencTermekek() {
   const { kedvencek } = useContext(ApiContext)
 
   return (
-    <div className="termekek-container">
+    <div className="row">
       {kedvencek.map((adat, index) => (
-        <Termek adat={adat} key={index} />
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={index}>
+          <Termek adat={adat} />
+        </div>
       ))}
     </div>
   )
