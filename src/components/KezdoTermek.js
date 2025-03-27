d83cacc61097062b53615ad0f1aefebd91433ad3
baksabence <gxdcloud@gmail.com>
2025-03-27 11:22:54 +0100

    istenem

--- src/components/KezdoTermek.js
+++ src/components/KezdoTermek.js
@@ -21,21 +21,31 @@ function Termek(props) {
     <div>
         <img
           className="kkep"
-          alt={props.adat.modell.tipus}
-          src={props.adat.modell.kep}
+          alt={props.adat.tipus}
+          src={props.adat.kep}
           onClick={handleImageClick}
           style={{ cursor: "pointer", width: '100%', height: 'auto' }}
         /> 
-        <p className="kar">{props.adat.ar} Ft</p>
+        <p className="kar">{props.adat.termekek.slice(0, 1).map(termek => 
+            termek.arak_megjelenit?.length > 0 
+              ? termek.arak_megjelenit[0].uj_ar 
+              : termek.ar
+          ).join(', ')} Ft</p>
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>{props.adat.tipus}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <img src={props.adat.modell.kep} alt={props.adat.modell.tipus} className="modal-img" style={{ aspectRatio: 19 / 23, objectFit: 'cover' }} />
-          <p>Kategória: {props.adat.modell.kategoria.ruhazat_kat}</p>
-          <p>Gyártó: {props.adat.modell.gyarto}</p>
-          <p>Ár: {props.adat.ar} Ft</p>
+          <img src={props.adat.kep} alt={props.adat.tipus} className="modal-img" style={{ aspectRatio: 19 / 23, objectFit: 'cover' }} />
+          <p>Kategória: {props.adat.kategoria.ruhazat_kat}</p>
+          <p>Gyártó: {props.adat.gyarto}</p>
+          <p className="ar card-text">
+          {props.adat.termekek.slice(0, 1).map(termek => 
+            termek.arak_megjelenit?.length > 0 
+              ? termek.arak_megjelenit[0].uj_ar 
+              : termek.ar
+          ).join(', ')} Ft
+        </p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>Bezárás</Button>
